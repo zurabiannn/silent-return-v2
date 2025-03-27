b12d42d4d2409a60d1d3383e035d61c51faf5e50
zurabiannn <ornella.zurabian@gmail.com>
2025-03-27 23:22:33 +0400

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Silent Return</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
